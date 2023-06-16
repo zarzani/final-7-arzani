af07449fac87175a88b3852cd896cfef12279acc
zahra <zahraarzani@gmail.com>
2023-06-16 09:44:13 +0330

    Api for different cities, temp, wind and humidity
    All working very well, it chacked for two cities (Seattle and Sydney

--- App.js
+++ App.js
@@ -10,5 +10,24 @@ let month = months[now.getMonth()];
 h3.innerHTML = (day)+"&nbsp" +" "+ (hour)+ ":"+ (minutes)+"&nbsp"+" " +(month)+":"+ (date);
 
 
+function showData(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#my-city");
+    let humElement = document.querySelector("#Humidity");
+   let windElement = document.querySelector("#wind");
+    
+   
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = (response.data.name);
+    humElement.innerHTML = ("Humidity"+ " " + response.data.main.humidity+" " +"%");
+    windElement.innerHTML = ("wind" + " " + response.data.wind.speed+ " "+ "Km/h");
 
+    }  
+
+
+let apiKey = "34f95b5e87d4683b0836302b1b590869";
+let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=34f95b5e87d4683b0836302b1b590869&units=metric";
+
+axios.get(apiUrl).then(showData);
 
